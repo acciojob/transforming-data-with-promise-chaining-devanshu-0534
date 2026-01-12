9c25bdd8b1da6958ab644c078f0e724e7dcf88d8
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-12 22:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 //your JS code here. If required.
+const input = document.getElementById("ip");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.onclick = function (){
+	const value = Number(input.value);
+	new Promise ((resolve)=>{
+		setTimeOut(()=>{
+			output.textContent = `Result:${value}`;
+			resolve(value);
+		}, 2000)
+	})
+	.then((num)=>{
+		return new Promise((resolve) => {
+			setTimeOut(()=>{
+				const result = num * 2;
+				output.textContent = `Result:${result}`;
+				resolve(result);
+			}, 1000)
+		})
+	})
+	.then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeOut(()=>{
+				const result = num - 3;
+				output.textContent = `Result: ${result}`;
+				resolve(result)
+			}, 1000)
+		})
+	})
+	.then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeOut(()=>{
+				const result = num/2;
+				output.textContent = `Result: ${result}`;
+				resolve(result);
+			}, 1000)
+		})
+	})
+	.then((num)=>{
+		return new Promise((resolve)=>{
+			setTimeOut(()=>{
+				const result = num + 10;
+				output.textContent = `Final Result: ${result}`;
+				resolve(result);
+			}, 1000)
+		})
+	})
+	
+}
+
+
+
+
+
+
+
+
+
